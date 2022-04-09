ab150bb20480129a7bc6448911d92f7641b49fac
Pham vAN mANH <pvm26042000@gmail.com>
2022-04-09 21:33:18 +0700

    push

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,11 @@
 FROM python:3.6
 WORKDIR /code
-COPY . .
-VOLUME /Users/phamvanmanh/Desktop/sentiment-analyst
+
 RUN pip install --upgrade pip
 RUN pip install tensorflow==2.6.0 -f https://tf.kmtea.eu/whl/stable.html
+COPY requirements.txt requirements.txt
 RUN pip3 install -r requirements.txt
+
 ENV FLASK_APP=app.py
 ENV FLASK_RUN_HOST=0.0.0.0
 EXPOSE 5000
--- app.py
+++ app.py
@@ -3,18 +3,21 @@ from processing.CleanData import CleanData
 from tensorflow.keras.preprocessing.sequence import pad_sequences
 from tensorflow.keras.preprocessing.text import one_hot
 import numpy as np
-from flask import Flask, request, jsonify
+from unittest import result
+from flask import Flask, request, jsonify, render_template
 
 VOCABSIZE =  30000
 MAX_SEQUENCE_LENGTH = 70
+print('setup and loading model')
 model = SentimentModel(model_path = './model/my_model')
 model.loading()
+print('---Done setup and loading model----')
 
 app = Flask(__name__)
 
-@app.route('/predict', methods=['GET', 'POST'])
-def predict():
 
+@app.route('/sentiment-analyst/predict', methods=['POST'])
+def predict():
     content = request.json
     text = content['text']
     clean_data = CleanData(text)
@@ -23,14 +26,20 @@ def predict():
     embedded_docs=pad_sequences(onehot_repr,padding='post',maxlen=MAX_SEQUENCE_LENGTH)
     
     X =np.array(embedded_docs)
-    value = model.predict(X)[0]
-    
-    if value > 0.5:
-        return jsonify({'result:': "day la mot cau tích cực: {}".format(value)})
-    else:
-        return jsonify({'result:': "day la mot cau tiêu cực: {}".format(value)})
+    value = model.predict(X)[0][0]
+    print("--------------")
+    print(value)
+    return jsonify(result = 1.*np.float32(value))
+
+@app.route('/sentiment-analyst', methods=['GET', 'POST'])
+def show1():
+    temple ='sentiment-analyst.html'
+    return render_template(temple)
+
+
+
     
 
 if __name__ == '__main__':
-    print('fuck')
+    print('starting!!!!')
     app.run(host= '0.0.0.0',debug=True)
